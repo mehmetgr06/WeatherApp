6e6f8e246c57bb7f10655ee9d1f761ddc4bf1ee8
mehmet_gur <mehmet.gur@getir.com>
2023-01-18 02:33:56 +0300

    weatherCard composable added

---
+++ app/src/main/java/com/example/weatherapp/presentation/components/WeatherCard.kt
@@ -0,0 +1,73 @@
+package com.example.weatherapp.presentation.components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.presentation.WeatherState
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    state.weatherInfo?.currentWeatherData?.let { weatherData ->
+        Card(
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(12.dp),
+            modifier = modifier.padding(16.dp)
+        ) {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today ${
+                        weatherData.time.format(
+                            DateTimeFormatter.ofPattern("HH:mm")
+                        )
+                    }",
+                    modifier = modifier.align(Alignment.End),
+                    color = Color.White
+                )
+                Spacer(modifier = modifier.height(16.dp))
+                Image(
+                    painter = painterResource(id = weatherData.weatherType.iconRes),
+                    contentDescription = "Weather Type"
+                )
+                Spacer(modifier = modifier.height(16.dp))
+                Text(
+                    text = "${weatherData.temperatureCelsius}°C",
+                    fontSize = 52.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = modifier.height(16.dp))
+                Text(
+                    text = weatherData.weatherType.weatherDesc,
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+            }
+        }
+    }
+}

d0814242feb6a2b1272575da0927719a9794a9b6
mehmet_gur <mehmet.gur@getir.com>
2023-01-18 03:21:57 +0300

    hourlyWeatherColumn composable added

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.weatherapp.presentation
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.example.weatherapp.presentation.components.WeatherCard
+import com.example.weatherapp.presentation.ui.theme.DarkBlue
+import com.example.weatherapp.presentation.ui.theme.DeepBlue
 import com.example.weatherapp.presentation.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,6 +25,7 @@ class MainActivity : ComponentActivity() {
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         permissionLauncher = registerForActivityResult(
@@ -31,7 +41,16 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             WeatherAppTheme {
-
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(DarkBlue)
+                ) {
+                    WeatherCard(
+                        state = viewModel.state,
+                        backgroundColor = DeepBlue
+                    )
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/weatherapp/presentation/components/HourlyWeatherColumn.kt
@@ -0,0 +1,52 @@
+package com.example.weatherapp.presentation.components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.weatherapp.domain.model.WeatherData
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun HourlyWeatherDisplay(
+    weatherData: WeatherData,
+    modifier: Modifier = Modifier,
+    textColor: Color = Color.White
+) {
+    // here we used remember to not calculating dateFormat everyTime
+    // with remember it will be calculated only if weatherData is changed
+    val formattedTime = remember(weatherData) {
+        weatherData.time.format(
+            DateTimeFormatter.ofPattern("HH:mm")
+        )
+    }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = formattedTime, color = Color.LightGray)
+        Image(
+            painter = painterResource(id = weatherData.weatherType.iconRes),
+            contentDescription = null,
+            modifier = modifier.width(40.dp)
+        )
+        Text(
+            text = "${weatherData.temperatureCelsius}°C",
+            color = textColor,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
